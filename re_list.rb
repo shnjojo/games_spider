fcd4f2582ac03f1c05fcb87fd87f1f0eaf88539d
JoJo Hsu <jojo.hsuu@gmail.com>
2014-08-14 15:09:34 +0800

    main logic of the games data on list page  finished.

--- re_list.rb
+++ re_list.rb
@@ -7,20 +7,21 @@ seeder_url = Nokogiri::HTML(open('http://tvgdb.duowan.com/ps4'))
 last_page_url = seeder_url.css('.mod-page-bd a.last')[0]['href']
 
 # Get last page num by regex
-page_position = last_page_url =~ /page=/
-last_page_num = last_page_url[(page_position + 5)..-1]
+last_page_position = last_page_url =~ /page=/
+last_page_num = last_page_url[(last_page_position + 5)..-1]
 
-# Array for all the game info on list
-list_game_info = []
+# Array for all the games info on list
+list_games_info = []
 
 def getGameOnList(page_num)
   current_page_url = 'http://tvgdb.duowan.com/ps4' + '?page=' + page_num.to_s
   current_page = Nokogiri::HTML(open(current_page_url))
+  list_game_info = []
   
   # Get game id & game region from game url
-  game_info = {}
   games_url = current_page.css('h4 a')
   games_url.each do |game_url|
+    game_info = {}
     game_id_position = game_url['href'] =~ /ps4/
     game_id = game_url['href'][game_id_position + 4..-8]
     game_region_position = game_url['href'] =~ /html/
@@ -28,12 +29,16 @@ def getGameOnList(page_num)
     game_info['id'] = game_id
     game_info['region'] = game_region
     
-    # From here to finish the Hash of game info.
+    list_game_info << game_info
   end
+
+  return list_game_info
+
 end
 
-getGameOnList(1)
+(1..last_page_num.to_i).each do |num|
+  list_games_info = list_games_info + getGameOnList(num)
+end
 
-#(1..last_page_num.to_i).each do |num|
-  #getEachGame(num)
-  #end
+print list_games_info
+puts list_games_info.count
